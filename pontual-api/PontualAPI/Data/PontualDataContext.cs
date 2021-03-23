26df26c7c94c0f2ad46d4eabf7b18d04d6f2d627
Phelip <pheliplogann@gmail.com>
2021-03-23 20:47:19 -0300

    alterações pontuais nos métodos de alteração e criação

---
+++ pontual-api/PontualAPI/Data/PontualDataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PontualAPI.Config;
+using PontualAPI.Models;
+
+namespace PontualAPI.Data
+{
+    public class PontualDataContext : DbContext
+    {
+        public DbSet<Cliente> Clientes { get; set; }
+        public PontualDataContext(DbContextOptions<PontualDataContext> options) : base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            new ClienteConfig().Configure(modelBuilder.Entity<Cliente>());
+        }
+    }
+}
